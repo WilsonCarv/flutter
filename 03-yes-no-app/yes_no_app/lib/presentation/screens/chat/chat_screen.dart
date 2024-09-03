d2c18b925ed23aa374f543c9797bdc20d4ac7520
Wilson Carvajal <wilsoncarvajal@Wilsons-MacBook-Pro.local>
2024-09-03 08:54:29 -0600

    working on chat

---
+++ 03-yes-no-app/yes_no_app/lib/presentation/screens/chat/chat_screen.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+
+class ChatScreen extends StatelessWidget {
+  const ChatScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: const Padding(
+          padding: EdgeInsets.all(4.0),
+          child: CircleAvatar(
+            backgroundImage: NetworkImage('https://i.pravatar.cc/300'),
+          ),
+        ),
+        title: const Text('Dude'),
+        centerTitle: false,
+      ),
+      body: _ChatView(),
+    );
+  }
+}
+
+class _ChatView extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 10),
+        child: Column(
+          children: [
+            Expanded(
+                child: ListView.builder(
+              itemCount: 100,
+              itemBuilder: (context, index) {
+                return Text('index $index');
+              },
+            )),
+            Text('Hello'),
+          ],
+        ),
+      ),
+    );
+  }
+}
