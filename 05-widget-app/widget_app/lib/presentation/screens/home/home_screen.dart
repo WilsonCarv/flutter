9f322ba35ae9d9eb9efd806a9d7d724e2225c098
Wilson Carvajal <wilsoncarvajal@Wilsons-MacBook-Pro.local>
2024-09-05 15:29:27 -0600

    home screen

--- 05-widget-app/widget_app/lib/presentation/screens/home/home_screen.dart
+++ 05-widget-app/widget_app/lib/presentation/screens/home/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:widget_app/config/menu/menu_items.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -9,6 +10,42 @@ class HomeScreen extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Flutter Widgets'),
       ),
+      body: _HomeView(),
     );
+  }
+}
+
+class _HomeView extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      itemCount: menuItems.length,
+      itemBuilder: (context, index) {
+        final menuItem = menuItems[index];
+        return _CustomListItem(menuItem: menuItem);
+      },
+    );
+  }
+}
+
+class _CustomListItem extends StatelessWidget {
+  const _CustomListItem({
+    required this.menuItem,
+  });
+
+  final MenuItem menuItem;
+
+  @override
+  Widget build(BuildContext context) {
+    final colors = Theme.of(context).colorScheme;
+
+    return ListTile(
+        title: Text(menuItem.title),
+        subtitle: Text(menuItem.subTitle),
+        leading: Icon(menuItem.icon, color: colors.primary),
+        trailing: Icon(Icons.chevron_right, color: colors.primary),
+        onTap: () {
+          //TODO: Navigate
+        });
   }
 }
