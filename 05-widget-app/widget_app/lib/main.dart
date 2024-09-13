87a474b1474765c147f4f76bb53d8cca5f4a0dcf
Wilson Carvajal <wilsoncarvajal@Wilsons-MacBook-Pro.local>
2024-09-12 19:22:13 -0600

    counter riverpod

--- 05-widget-app/widget_app/lib/main.dart
+++ 05-widget-app/widget_app/lib/main.dart
@@ -15,7 +15,7 @@ class MainApp extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-        final AppTheme appTheme = ref.watch( themeNotifierProvider );
+    final AppTheme appTheme = ref.watch(themeNotifierProvider);
     return MaterialApp.router(
       routerConfig: appRouter,
       debugShowCheckedModeBanner: false,
