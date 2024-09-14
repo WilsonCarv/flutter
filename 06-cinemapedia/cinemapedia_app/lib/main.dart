3a3b238421e98acfde51cb2fea9b0c7f6e605047
Wilson Carvajal <wilsoncarvajal@Wilsons-MacBook-Pro.local>
2024-09-14 14:31:38 -0600

    add enviroment variables

--- 06-cinemapedia/cinemapedia_app/lib/main.dart
+++ 06-cinemapedia/cinemapedia_app/lib/main.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:cinemapedia_app/config/router/app_router.dart';
 import 'package:cinemapedia_app/config/theme/app_theme.dart';
-
-void main() {
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+Future<void> main() async {
+  await dotenv.load(fileName: '.env');
   runApp(const MainApp());
 }
 
