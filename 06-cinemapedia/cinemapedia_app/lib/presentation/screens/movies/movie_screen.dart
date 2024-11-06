8e4347dda32ec7f8cf70e660865ad28a3228fe01
Wilson Carvajal <wilsoncarvajal@Wilsons-MacBook-Pro.local>
2024-11-06 12:31:25 -0600

    loading on movie details

--- 06-cinemapedia/cinemapedia_app/lib/presentation/screens/movies/movie_screen.dart
+++ 06-cinemapedia/cinemapedia_app/lib/presentation/screens/movies/movie_screen.dart
@@ -1,4 +1,3 @@
-import 'package:cinemapedia_app/domain/entities/movie.dart';
 import 'package:cinemapedia_app/presentation/providers/providers.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
