96a69095f65ddd3f426c0087752317d4a5ae4040
Wilson Carvajal <wilsoncarvajal@Wilsons-MacBook-Pro.local>
2024-11-07 14:08:22 -0600

    navigate to route

--- 06-cinemapedia/cinemapedia_app/lib/infraestructure/datasources/movidedb_datasource.dart
+++ 06-cinemapedia/cinemapedia_app/lib/infraestructure/datasources/movidedb_datasource.dart
@@ -86,6 +86,7 @@ class MovidedbDatasource extends MoviesDatasource {
   
   @override
   Future<List<Movie>> searchMovies(String query) async {
+    if(query.isEmpty) return [];
     final response = await dio.get('/search/movie', 
       queryParameters: {
         'query': query
