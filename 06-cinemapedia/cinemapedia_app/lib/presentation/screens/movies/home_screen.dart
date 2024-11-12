b0ffbcc8c95a6c697363c82006c602689f21de78
Wilson Carvajal <wilsoncarvajal@Wilsons-MacBook-Pro.local>
2024-11-12 17:09:39 -0600

    use views

--- 06-cinemapedia/cinemapedia_app/lib/presentation/screens/movies/home_screen.dart
+++ 06-cinemapedia/cinemapedia_app/lib/presentation/screens/movies/home_screen.dart
@@ -1,7 +1,6 @@
+import 'package:cinemapedia_app/presentation/views/views.dart';
+import 'package:cinemapedia_app/presentation/widgets/shared/custom_bottom_navigationbar.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:cinemapedia_app/presentation/providers/providers.dart';
-import 'package:cinemapedia_app/presentation/widgets/widgets.dart';
 
 class HomeScreen extends StatelessWidget {
   static const name = 'home_screen';
@@ -10,98 +9,9 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const Scaffold(
-      body: _HomeView(),
+      body: Center(
+        child: Text('Home'),),
       bottomNavigationBar: CustomBottomNavigation(),
-    );
-  }
-}
-
-class _HomeView extends ConsumerStatefulWidget {
-  const _HomeView();
-
-  @override
-  _HomeViewState createState() => _HomeViewState();
-}
-
-class _HomeViewState extends ConsumerState<_HomeView> {
-  @override
-  void initState() {
-    super.initState();
-
-    ref.read(nowPlayingMoviesProvider.notifier).loadNextPage();
-     ref.read( popularMoviesProvider.notifier ).loadNextPage();
-    ref.read( topRatedMoviesProvider.notifier ).loadNextPage();
-    ref.read( upcomingMoviesProvider.notifier ).loadNextPage();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    final initialLoading = ref.watch(initialLoadingProvider);
-    if ( initialLoading ) return const FullScreenLoader();
-    
-    final slideShowMovies = ref.watch( moviesSlideShowProvider );
-    final nowPlayingMovies = ref.watch( nowPlayingMoviesProvider );
-    final popularMovies = ref.watch( popularMoviesProvider );
-    final topRatedMovies = ref.watch( topRatedMoviesProvider );
-    final upcomingMovies = ref.watch( upcomingMoviesProvider );
-
-    return CustomScrollView(
-      slivers: [
-
-        const SliverAppBar(
-          floating: true,
-          flexibleSpace: FlexibleSpaceBar(
-            title: CustomAppBar(),
-          ),
-        ),
-
-
-        SliverList(delegate: SliverChildBuilderDelegate(
-          (context, index) {
-              return Column(
-                  children: [
-              
-                    MoviesSlideshow(movies: slideShowMovies ),
-              
-                    MovieHorizontalListview(
-                      movies: nowPlayingMovies,
-                      title: 'Playing now',
-                      subTitle: 'Monday 20th',
-                      loadNextPage: () =>ref.read(nowPlayingMoviesProvider.notifier).loadNextPage()
-                      
-                    ),
-              
-                    MovieHorizontalListview(
-                      movies: upcomingMovies,
-                      title: 'Next',
-                      subTitle: 'This month',
-                      loadNextPage: () =>ref.read(upcomingMoviesProvider.notifier).loadNextPage()
-                    ),
-              
-                    MovieHorizontalListview(
-                      movies: popularMovies,
-                      title: 'Popular',
-                      // subTitle: '',
-                      loadNextPage: () =>ref.read(popularMoviesProvider.notifier).loadNextPage()
-                    ),
-              
-                    MovieHorizontalListview(
-                      movies: topRatedMovies,
-                      title: 'Best rated',
-                      subTitle: 'Today',
-                      loadNextPage: () =>ref.read(topRatedMoviesProvider.notifier).loadNextPage()
-                    ),
-
-                    const SizedBox( height: 10 ),
-              
-              
-                  ],
-                );
-          },
-          childCount: 1
-        )),
-
-      ]
     );
   }
 }
